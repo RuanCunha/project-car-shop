bab6279eafb3e85556351aa0af173a660ea06f1d
RuanCunha <ruanvcunha@hotmail.com>
2022-07-25 05:03:21 -0300

    test: 💍 implement model layer tests

---
+++ src/tests/unit/controllers/carController.test.ts
@@ -0,0 +1,31 @@
+// template para criação dos testes de cobertura da camada de controller
+
+
+import sinon, { SinonStub } from 'sinon';
+import chai, { expect } from 'chai';
+import chaiHttp = require('chai-http');
+
+describe('Car Controller', () => {
+  
+});
+
+
+// chai.use(chaiHttp);
+
+// const { expect } = chai;
+
+// describe('Sua descrição', () => {
+
+//   before(async () => {
+//     sinon
+//       .stub()
+//       .resolves();
+//   });
+
+//   after(()=>{
+//     ().restore();
+//   })
+
+//   it('', async () => {});
+
+// });
